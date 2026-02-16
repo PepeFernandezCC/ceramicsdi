78d337e45dad878abb97a7d6edd871450a9354cf
PepeFernandezCC <pepe.fernandez@ceramicconnection.es>
2026-02-16 13:03:52 +0100

    block emails

--- override/classes/Mail.php
+++ override/classes/Mail.php
@@ -21,7 +21,7 @@ class Mail extends MailCore
         $replyToName = null
     ) {
 
-        $block_templates = ['order_conf', 'bankwire', 'cheque'];
+        $block_templates = ['order_conf', 'cheque'];
         // Bloquear solo confirmación de pedido
         if (in_array($template, $block_templates)) {
             return true; // "simula" enviado para no romper flujos
